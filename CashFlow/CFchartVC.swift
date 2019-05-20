4e28df31631900f5ac7a15ad5c276470324e78a5
StevenGardnerGMJ <gemaojing1234@126.com>
2019-05-20 18:00:24 +0800

    统计图1

---
+++ CashFlow/CFchartVC.swift
@@ -0,0 +1,23 @@
+//
+//  CFchartVC.swift
+//  CashFlow
+//
+//  Created by David on 2019/5/20.
+//  Copyright © 2019 葛茂菁. All rights reserved.
+//
+
+import UIKit
+
+class CFchartVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //饼状统计数据
+    // A 雷达  B饼状  C 饼状
+    }
+    
+
+   
+
+}
